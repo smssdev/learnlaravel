39922aa694a5fc7749070a04846c7b5ef682a8db
Solayman Salah <slieman2001@hotmail.com>
2025-04-24 12:34:22 +0200

    ..

---
+++ test/resources/views/new_page.blade.php
@@ -0,0 +1,1 @@
+<h1>Hi new Page</h1>
--- test/routes/web.php
+++ test/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', function () {
+    return "Welcome";
+});
+
+Route::get('/new_page', function () {
+    return view("new_page");
+});
