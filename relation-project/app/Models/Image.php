6d95a1685d5de643dbf28194ce2a9c8b29d20050
Solayman Salah <slieman2001@hotmail.com>
2025-04-25 16:54:41 +0300

    Image model

--- relation-project/app/Models/Image.php
+++ relation-project/app/Models/Image.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Image extends Model
 {
-    //
+    protected $fillable = ['url'];
+
+    public function imageable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
