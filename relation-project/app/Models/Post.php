c9b0b0bbe3e28708e65858d907e343a5830891eb
Solayman Salah <slieman2001@hotmail.com>
2025-04-25 16:53:24 +0300

    post model

--- relation-project/app/Models/Post.php
+++ relation-project/app/Models/Post.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['user_id', 'title', 'content'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function images(): MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
