c3674f3d06571448e7fb59ee769af9bcd1956a3c
Solayman Salah <slieman2001@hotmail.com>
2025-04-25 16:52:28 +0300

    profile model

--- relation-project/app/Models/Profile.php
+++ relation-project/app/Models/Profile.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Profile extends Model
 {
-    //
+    protected $fillable = ['user_id', 'phone', 'address'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
