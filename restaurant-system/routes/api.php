cd0f4ff9bee796361577f27577dbe75d6de3d15b
Solayman Salah <slieman2001@hotmail.com>
2025-04-26 21:34:21 +0300

    middleware not work

--- restaurant-system/routes/api.php
+++ restaurant-system/routes/api.php
@@ -4,12 +4,17 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 });
+
+// Route::middleware('CheckUser')->get('/user2', function (Request $request) {
+//     return response()->json(['message' => 'User authenticated']);
+// });
+
