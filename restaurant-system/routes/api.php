32181ab710e56da84f444e9b396e49eba01d699b
Solayman Salah <slieman2001@hotmail.com>
2025-04-26 21:15:52 +0300

    auth login,register, logout

--- restaurant-system/routes/api.php
+++ restaurant-system/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
+});
